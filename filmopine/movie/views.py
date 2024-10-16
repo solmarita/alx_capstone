794ff7c887df5be28848ad68c87d8a2df82e3375
Solomon Marita <officialmokua@gmail.com>
2024-10-16 18:39:20 +0300

    Refactored MovieSearchView to add pagination for results, both with or without query being provided

--- filmopine/movie/views.py
+++ filmopine/movie/views.py
@@ -2,7 +2,7 @@ import requests
 from decouple import config
 from django.shortcuts import render
 from rest_framework import viewsets
-from rest_framework import status
+from rest_framework.pagination import PageNumberPagination
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import Movie
@@ -12,20 +12,27 @@ class MovieViewSet(viewsets.ModelViewSet):
     queryset = Movie.objects.all()
     serializer_class = MovieSerializer
 
+class MovieSearchPagination(PageNumberPagination):
+    page_size = 10  # Number of items per page
+    page_size_query_param = 'page_size'  # Allow clients to set the page size
+    max_page_size = 100  # Maximum limit for page size
+
 class MovieSearchView(APIView):
     OMDB_API_KEY = config('OMDB_API_KEY')
     OMDB_BASE_URL = 'http://www.omdbapi.com/'
+    paginator = MovieSearchPagination()
 
     def get(self, request):
         query = request.query_params.get('query')
 
         # If no query is provided, return all movies
         if not query:
-            # Optionally, you can return a message or a list of all movies
+            # Retrieve all movies and paginate
             movies = Movie.objects.all()
-            serializer = MovieSerializer(movies, many=True)
-            return Response(serializer.data, status=200)
+            paginated_movies = self.paginator.paginate_queryset(movies, request)
 
+            serializer = MovieSerializer(paginated_movies, many=True)
+            return self.paginator.get_paginated_response(serializer.data)
         # Send a request to the OMDb API
         url = f"{self.OMDB_BASE_URL}?apikey={self.OMDB_API_KEY}&s={query}"
         omdb_response = requests.get(url).json()
@@ -57,4 +64,7 @@ class MovieSearchView(APIView):
             serializer = MovieSerializer(movie)
             movies.append(serializer.data)
 
-        return Response(movies, status=200)
+        # Paginate the results
+        paginated_movies = self.paginator.paginate_queryset(movies, request)
+
+        return self.paginator.get_paginated_response(paginated_movies)
