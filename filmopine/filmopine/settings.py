84d585dc5d0ab989d2bdc3d811a8427c584158fa
Solomon Marita <officialmokua@gmail.com>
2024-10-19 13:40:24 +0300

    Set STATIC_ROOT and create staticfiles directory

--- filmopine/filmopine/settings.py
+++ filmopine/filmopine/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
 from decouple import config
 from datetime import timedelta
@@ -135,6 +136,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
 STATIC_URL = 'static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # This is where static files will be collected
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
